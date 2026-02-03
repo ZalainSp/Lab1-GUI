72064404e5d3180503e5b3a4c69806c51f940d19
Zalain Spain <2023159874@ub.edu.bz>
2026-02-03 15:35:24 -0600

    added comments

--- exercise8.js
+++ exercise8.js
@@ -1,19 +1,19 @@
-const shape = {
-    calculateArea(){
+const shape = { //base object
+    calculateArea(){ //default method
         return 0;
     }
 };
 
-const rectangle = Object.create(shape);
-rectangle.width = 10;
-rectangle.height = 5;
-rectangle.calculateArea = function() {
+const rectangle = Object.create(shape); //new object created that is inheriting from shape
+rectangle.width = 10; //adding properties to rectangle
+rectangle.height = 5; //adding properties to rectangle
+rectangle.calculateArea = function() { //overriding calculateArea method for rectangle
     return this.width * this.height;
 }
 
-const circle = Object.create(shape);
-circle.radius = 5;
-circle.calculateArea = function() {
+const circle = Object.create(shape); //new object created that is inheriting from shape
+circle.radius = 5; //adding properties to circle
+circle.calculateArea = function() { //overriding calculateArea method for circle
     return Math.PI * this.radius * this.radius;
 }
 
