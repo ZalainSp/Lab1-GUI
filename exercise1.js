f5eb1c083673667767ebf898f89086d3aabece55
Zalain Spain <2023159874@ub.edu.bz>
2026-02-03 13:39:18 -0600

    Completed Part : Function declarations and expressions

---
+++ exercise1.js
@@ -0,0 +1,7 @@
+function caclulateArea(length , width) {
+    return length * width;
+}
+
+console.log(caclulateArea(2, 4)); //8
+console.log(caclulateArea(5, 10)) //80
+console.log(caclulateArea(7, 3)); //21
---
+++ exercise2.js
@@ -0,0 +1,7 @@
+const calculatePerimiter = function(length, width) {
+    return (2 * length) + (2 * width);
+}
+
+console.log(calculatePerimiter(2 , 2)); //8
+console.log(calculatePerimiter(5 , 10)); //30
+console.log(calculatePerimiter(7 , 3)); //20
